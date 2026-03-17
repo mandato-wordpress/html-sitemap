d808a9447162fb74b0889ebef9974933f3991ed9
Angelo Mandato <3031332+angelomandato@users.noreply.github.com>
2026-03-17 16:48:01 -0400

    - Latest build of block.

--- build/html-sitemap-block/index.asset.php
+++ build/html-sitemap-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'dd2f1949e91d3dbf9d0d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '462c7eadb567dc7b92e9');
